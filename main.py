05abb9aa9a1cc1775b8453c001cb823460507391
Luvdduk <66945720+Luvdduk@users.noreply.github.com>
2020-12-11 04:17:49 +0900

    .

--- main.py
+++ main.py
@@ -101,24 +101,24 @@ def fan_speedsw(): # 팬 스피드 조절, 팬속버튼 누를때마다 실행
         if fan_state == "SLOW": # 팬속도가 1단계일때 2단계로 변경
             fan_pwm.value = 0.65
             fan_state = "MID"
-            conf['FAN']['fan_speed'] = fan_state
-            with open('config.ini', 'w') as configfile:
+            conf['FAN']['fan_speed'] = fan_state #팬 상태 저장
+            with open('config.ini', 'w') as configfile: # 파일 쓰기
                 conf.write(configfile)
             print("팬속도: %f"%fan_pwm.value)
             return
         if fan_state == "MID": # 팬속도가 2단계일때 3단계로 변경
             fan_pwm.value = 1
             fan_state = "FULL"
-            conf['FAN']['fan_speed'] = fan_state
-            with open('config.ini', 'w') as configfile:
+            conf['FAN']['fan_speed'] = fan_state #팬 상태 저장
+            with open('config.ini', 'w') as configfile:# 파일 쓰기
                 conf.write(configfile)
             print("팬속도: %f"%fan_pwm.value)
             return
         if fan_state == "FULL": # 팬속도가 3단계일때 1단계로 변경
             fan_pwm.value = 0.3
             fan_state = "SLOW"
-            conf['FAN']['fan_speed'] = fan_state
-            with open('config.ini', 'w') as configfile:
+            conf['FAN']['fan_speed'] = fan_state #팬 상태 저장
+            with open('config.ini', 'w') as configfile: # 파일 쓰기
                 conf.write(configfile)
             print("팬속도: %f"%fan_pwm.value)
             return
@@ -141,14 +141,14 @@ def display_dust(duststate1, duststate2, duststate3):
     global power_state, fan_state
     # 좋음
     if duststate3 <= 30 and (duststate1 + duststate2) <= 15 :
-        lcd.lcd_string("     GOOD      ", lcd.LCD_LINE_1)
+        lcd.lcd_string("     GOOD      ", lcd.LCD_LINE_1) # lcd에 good표시
         led.color = Color("blue") # led 색 지정 파랑
-        if power_state == 2: #자동모드 팬 속도 30%
+        if power_state == 2: #자동모드 팬 끄기
             fan_power(OFF)
             
     # 보통
     elif  duststate3 <= 80 and (duststate1 + duststate2) <= 35:
-        lcd.lcd_string("     NORMAL    ", lcd.LCD_LINE_1)
+        lcd.lcd_string("     NORMAL    ", lcd.LCD_LINE_1)# lcd에 normal표시
         led.color = Color("green") # led 색 지정 초록
         if power_state == 2: #자동모드 팬 속도 30%
             fan_power(ON)
@@ -156,7 +156,7 @@ def display_dust(duststate1, duststate2, duststate3):
             fan_state = "SLOW"
     # 나쁨
     elif duststate3 <= 150 and (duststate1 + duststate2) <= 75:
-        lcd.lcd_string("      BAD      ", lcd.LCD_LINE_1)
+        lcd.lcd_string("      BAD      ", lcd.LCD_LINE_1)# lcd에 bad표시
         led.color = Color("yellow") # led 색 지정 노랑
         if power_state == 2:#자동모드 팬 속도 65%
             fan_power(ON)
@@ -164,7 +164,7 @@ def display_dust(duststate1, duststate2, duststate3):
             fan_state = "MID"
     # 매우나쁨
     elif duststate3 > 150 or (duststate1 + duststate2) > 75:
-        lcd.lcd_string("    VERY BAD   ", lcd.LCD_LINE_1)
+        lcd.lcd_string("    VERY BAD   ", lcd.LCD_LINE_1)# lcd에 verybad표시
         led.color = Color("red") # led 색 지정 빨강
         if power_state == 2: #자동모드 팬 속도 100%
             fan_power(ON)
@@ -228,19 +228,20 @@ def loop():
             print("리퀘스트 수신 에러")
             print("#################")
         
-        if order['power_on'] == True: # 수신값 받아서 설정 변경
+        # 수신값 받아서 설정 변경
+        if order['power_on']: # 전원 켬
             power_state = 1
-        if order['power_off']:
+        if order['power_off']: # 전원 끔
             power_state = 0
-        if order['auto_mode']:
+        if order['auto_mode']: # 자동모드
             power_state = 2
-        if order['fan_slow']:
+        if order['fan_slow']: # 팬 30%
             fan_state = "SLOW"
             fan_pwm.value = 0.3
-        if order['fan_mid']:
+        if order['fan_mid']: # 팬 65%
             fan_state = "MID"
             fan_pwm.value = 0.65
-        if order['fan_full']:
+        if order['fan_full']: # 팬 100%
             fan_state = "FULL"
             fan_pwm.value = 1
         
@@ -249,11 +250,11 @@ def loop():
             conf.write(configfile)
 
         send_data = { #보낼 데이터
-            'power_state':power_state,
-            'fan_state': fan_state,
-            'pm1' : pm1,
-            'pm25' : pm25,
-            'pm10' : pm10
+            'power_state':power_state, # 전원변수
+            'fan_state': fan_state, # 팬속변수
+            'pm1' : pm1, # 먼지농도
+            'pm25' : pm25, # 먼지농도
+            'pm10' : pm10 # 먼지농도
         }
         # 서버통신 - 데이터 전송
         send_data = json.dumps(send_data) # json타입으로 변경
@@ -273,14 +274,14 @@ def loop():
             fan_power(OFF) #팬 off
             led.off() #led off
             lcd.LCD_BACKLIGHT = 0x00 #lcd 백라이트 off
-            lcd.lcd_string("   Power Off   ", lcd.LCD_LINE_1)
+            lcd.lcd_string("   Power Off   ", lcd.LCD_LINE_1) # lcd 윗줄에 poweroff 표시
             lcd.lcd_string("", lcd.LCD_LINE_2)
         if power_state == 1:
             print("전원켜짐")
             fan_power(ON) #팬 on
             # led.on() #led on
             lcd.LCD_BACKLIGHT = 0x08 #lcd 백라이트 on
-            lcd.lcd_string("   Power On    ", lcd.LCD_LINE_1)
+            lcd.lcd_string("   Power On    ", lcd.LCD_LINE_1)# lcd 윗줄에 poweron 표시
             lcd.lcd_string("", lcd.LCD_LINE_2)
             time.sleep(1)
             display_dust(pm1, pm25, pm10) #매개변수로 데이터 전달
@@ -288,7 +289,7 @@ def loop():
             print("자동모드")
             # led.on() #led on
             lcd.LCD_BACKLIGHT = 0x08 #lcd 백라이트 on
-            lcd.lcd_string("   Auto Mode   ", lcd.LCD_LINE_1)
+            lcd.lcd_string("   Auto Mode   ", lcd.LCD_LINE_1)# lcd 윗줄에 automode 표시
             lcd.lcd_string("", lcd.LCD_LINE_2)
             time.sleep(1)
             display_dust(pm1, pm25, pm10) #매개변수로 데이터 전달
@@ -297,6 +298,6 @@ def loop():
 
 
 if __name__ == "__main__":
-    # 쓰레딩
+    # 멀티쓰레딩
     Thread(target=Button_Ctrl).start()
     Thread(target=loop).start()
